54c6e2f7ed88370800eb3b82107927f1481bb064
minky <minkyeong02@daum.net>
2023-11-16 22:44:17 +0900

    [init] Java 17/ Spring boot 3.1.5/ spring web, lombok, mysql driver, jpa, security

--- src/main/java/com/capstone/AreyouP/AreyouPApplication.java
+++ src/main/java/com/capstone/AreyouP/AreyouPApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KbmlApplication {
+public class AreyouPApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KbmlApplication.class, args);
+		SpringApplication.run(AreyouPApplication.class, args);
 	}
 
 }
