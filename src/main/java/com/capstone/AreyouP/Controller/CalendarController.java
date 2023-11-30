6c43401689e0b5278fc27e4f7fd3742eb0db56c1
minky <minkyeong02@daum.net>
2023-11-30 19:59:06 +0900

    [DELETE] Calendar api 삭제 예정

--- src/main/java/com/capstone/AreyouP/Controller/CalendarController.java
+++ src/main/java/com/capstone/AreyouP/Controller/CalendarController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/calendar")
 public class CalendarController {
 
-//    @GetMapping("/{month}") //월 단위 화면이 나왔을 경우
-//    public ResponseEntity<?> monthInfo(@PathVariable Integer month){
+//    @PostMapping("/{calendar_id}")
+//    public ResponseEntity<?> calendarInfo(@PathVariable String calendar_id){
 //
 //    }
 }
