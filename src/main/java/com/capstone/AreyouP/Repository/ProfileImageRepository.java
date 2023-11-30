a759119140dcd72f76f8c50bc31432de5eb03382
minky <minkyeong02@daum.net>
2023-12-01 01:42:54 +0900

    [init] 프로필 이미지 repository

---
+++ src/main/java/com/capstone/AreyouP/Repository/ProfileImageRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.AreyouP.Repository;
+
+import com.capstone.AreyouP.Domain.ProfileImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
+}
