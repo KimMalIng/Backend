da97b13aa38099f13bec1957b09c8f31cb7c6d58
minky <minkyeong02@daum.net>
2023-12-04 04:21:10 +0900

    [ADD] 분배된 일정

---
+++ src/main/java/com/capstone/AreyouP/Repository/SeperatedJobRepository.java
@@ -0,0 +1,8 @@
+package com.capstone.AreyouP.Repository;
+
+import com.capstone.AreyouP.Domain.SeperatedJob;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeperatedJobRepository extends JpaRepository<SeperatedJob, Long> {
+
+}
