aceddee8172fc52befcb9ae986d4c15eb3a6b698
minky <minkyeong02@daum.net>
2024-03-26 23:51:19 +0900

    [#1 feat] JobException 생성

---
+++ src/main/java/com/example/areyoup/errors/errorcode/JobErrorCode.java
@@ -0,0 +1,16 @@
+package com.example.areyoup.errors.errorcode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum JobErrorCode implements ErrorCode{
+
+    JOB_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 일정입니다."),
+    JOB_AUTHOR_MISMATCH(HttpStatus.FORBIDDEN, "해당 일정에 접근 권한이 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
