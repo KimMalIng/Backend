7dfad96d0d3b19e9d1dec3d31bd1905e2d3d7777
minky <minkyeong02@daum.net>
2024-04-02 00:55:41 +0900

    [fix] Builder 수정

--- src/main/java/com/example/areyoup/everytime/dto/EveryTimeResponseDto.java
+++ src/main/java/com/example/areyoup/everytime/dto/EveryTimeResponseDto.java
@@ -4,19 +4,14 @@ import com.example.areyoup.everytime.domain.EveryTimeJob;
 import com.example.areyoup.job.dto.JobResponseDto;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 @Getter
+@SuperBuilder
 public class EveryTimeResponseDto extends JobResponseDto {
 
     private final Integer dayOfTheWeek;
-
-
-    @Builder
-    public EveryTimeResponseDto(Long id, String name, Integer label, String startTime, String endTime, String estimatedTime, String day, boolean isPrivate, boolean isComplete, boolean isFixed, Integer dayOfTheWeek) {
-        super(id, name, label, startTime, endTime, estimatedTime, isComplete, isFixed);
-        this.dayOfTheWeek = dayOfTheWeek;
-    }
 
 
     public static EveryTimeResponseDto toDto(EveryTimeJob j){
@@ -34,18 +29,6 @@ public class EveryTimeResponseDto extends JobResponseDto {
                 .build();
     }
 
-    public static EveryTimeResponseDto toAdjustDto(EveryTimeJob j, LocalDate localDate) {
-        String day = String.valueOf(localDate).replace("-", ".");
-        return EveryTimeResponseDto.builder()
-                .name(j.getName())
-                .label(j.getLabel())
-                .startTime(j.getStartTime())
-                .endTime(j.getEndTime())
-                .estimatedTime(j.getEstimatedTime())
-                .isComplete(j.isComplete())
-                .day(day)
-                .build();
-    }
 
 }
 
