f73a079e7449ad66a9729469e80c7815565d43d7
X1n9fU <minkyeong0244@gmail.com>
2024-04-13 00:46:22 +0900

    [feat] Refreshtoken error 추가

--- src/main/java/com/example/areyoup/errors/errorcode/MemberErrorCode.java
+++ src/main/java/com/example/areyoup/errors/errorcode/MemberErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements ErrorCode{
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
     AUTHENTICATION_FAILED(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 옳지 않습니다."),
-    MEMBER_DUPLICATED(HttpStatus.BAD_REQUEST, "이미 존재하는 회원입니다.");
+    MEMBER_DUPLICATED(HttpStatus.BAD_REQUEST, "이미 존재하는 회원입니다."),
+
+    REFRESHTOKEN_ERROR(HttpStatus.NOT_FOUND, "해당 RefreshToken에 맞는 회원이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
