a5eb75cc591952cb8a006bd34e4a1db5c14161be
X1n9fU <minkyeong0244@gmail.com>
2024-04-13 21:25:38 +0900

    [#13 feat] OAuth2 Login profileImage Upload

--- src/main/java/com/example/areyoup/global/oAuth2/OAuth2SuccessHandler.java
+++ src/main/java/com/example/areyoup/global/oAuth2/OAuth2SuccessHandler.java
@@ -6,25 +6,27 @@ import com.example.areyoup.global.cookie.CookieUtils;
 import com.example.areyoup.global.jwt.TokenService;
 import com.example.areyoup.global.jwt.dto.JwtTokenDto;
 import com.example.areyoup.member.domain.Member;
+import com.example.areyoup.profileimage.domain.ProfileImage;
 import com.example.areyoup.member.repository.MemberRepository;
-import jakarta.servlet.ServletException;
+import com.example.areyoup.profileimage.repository.ProfileImageRepository;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
@@ -36,11 +38,15 @@ import static java.lang.System.getenv;
 @RequiredArgsConstructor
 @Component
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-
-    private final TokenService tokenService;
-    private final MemberRepository memberRepository;
     private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
     private final PasswordEncoder passwordEncoder;
+
+    private final TokenService tokenService;
+
+    private final MemberRepository memberRepository;
+    private final ProfileImageRepository profileImageRepository;
+
+
 
     Map<String, String> env = getenv();
     String host = env.get("HOST");
@@ -49,20 +55,13 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
             throws IOException {
 
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-
         log.info("OAuth2User = {}", oAuth2User);
-
-        String targetUrl;
-
         log.info("토큰 발행 시작");
 
         JwtTokenDto jwtTokenDto = processOAuth2User(oAuth2User); //회원가입 및 로그인
-
 //        CookieUtils.addCookie(response, "refreshToken" , jwtTokenDto.getRefreshToken(), 180);
 
-//        String url = determineTargetUrl(request, response, authentication);
-
-        targetUrl = UriComponentsBuilder.fromUriString("http://"+ host +":3000/logincheck")
+        String targetUrl = UriComponentsBuilder.fromUriString("http://"+ host +":3000/logincheck")
                 .queryParam("accessToken",jwtTokenDto.getAccessToken())
                 .queryParam("refreshToken", jwtTokenDto.getRefreshToken())
                 .build().toUriString();
@@ -85,8 +84,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
 
 
-    @Transactional
-    public JwtTokenDto processOAuth2User(OAuth2User oAuth2User) throws JSONException {
+    public JwtTokenDto processOAuth2User(OAuth2User oAuth2User) throws JSONException, IOException {
         Map<String, Object> attributes = oAuth2User.getAttributes();
         String email = (String) attributes.get("email");
         String name = (String) attributes.get("name");
@@ -100,26 +98,36 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
                     .memberPw(pw)
                     .name(name)
                     .roles(Collections.singletonList("USER"))
+                    .profileImg(getProfileImage(oAuth2User))
                     .loginType("kakao")
                     .build();
-
             memberRepository.save(member);
 
             jwtTokenDto = tokenService.signIn(email, pw);
             member.toUpdateRefreshToken(jwtTokenDto.getRefreshToken());
-
-            log.info("OAuth Login 회원가입 완료");
-
+            log.info("OAuth2 Join Success");
         } else {
             jwtTokenDto = tokenService.signIn(email, userOptional.get().getMemberPw());
             //token 발행
         }
-        log.info("request id = {}", email);
-        log.info("Auth2 JWT : accessToken = {}, refreshToken = {}",
-                jwtTokenDto.getAccessToken(), jwtTokenDto.getRefreshToken());
-
+        log.info("OAuth2 request id = {}", email);
+        log.info("OAuth2 JWT success");
         return jwtTokenDto;
 
+    }
+
+    private ProfileImage getProfileImage(OAuth2User oAuth2User) throws IOException {
+        String picUrl = (String) oAuth2User.getAttributes().get("picture");
+        URL urlInput = new URL(picUrl);
+        BufferedImage urlImage = ImageIO.read(urlInput);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ImageIO.write(urlImage, "jpg", bos);
+        byte[] file = bos.toByteArray();
+
+        ProfileImage profileImage = new ProfileImage();
+        profileImage.toUpdateDate(file);
+        profileImageRepository.save(profileImage);
+        return profileImage;
     }
 
     private static String generateRandomString() {
