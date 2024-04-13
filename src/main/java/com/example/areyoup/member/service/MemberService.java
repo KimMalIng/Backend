7fa5c348086f0330deeabdac7bfcffe2e67a4ef4
X1n9fU <minkyeong0244@gmail.com>
2024-04-13 15:59:28 +0900

    [feat] if nickname? name = nickname

--- src/main/java/com/example/areyoup/member/service/MemberService.java
+++ src/main/java/com/example/areyoup/member/service/MemberService.java
@@ -11,6 +11,7 @@ import com.example.areyoup.member.repository.ProfileImageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StreamUtils;
@@ -26,6 +27,7 @@ import java.util.Collections;
 public class MemberService {
     private final MemberRepository memberRepository;
     private final ProfileImageRepository profileImageRepository;
+    private final PasswordEncoder passwordEncoder;
 
     private static final String PROFILE = "static\\images\\logo.png";
 
@@ -36,12 +38,13 @@ public class MemberService {
     public MemberResponseDto.MemberJoinDto join(MemberRequestDto.MemberJoinDto memberJoinDto){
         if (memberRepository.findByMemberId(memberJoinDto.getMemberId()).isPresent())
             throw new MemberException(MemberErrorCode.MEMBER_DUPLICATED);
+        String name = memberJoinDto.getNickname().isEmpty()? memberJoinDto.getName(): memberJoinDto.getNickname();
         ProfileImage image = new ProfileImage();
         imageUpload(memberJoinDto, image); //이미지 유무 판단 후 알맞는 사진 저장
         Member member = Member.builder()
                 .memberId(memberJoinDto.getMemberId())
-                .memberPw(memberJoinDto.getMemberPw())
-                .name(memberJoinDto.getName())
+                .memberPw(passwordEncoder.encode(memberJoinDto.getMemberPw()))
+                .name(name) //nickname이 있으면 반환, 아니면 그냥 이름 적용
                 .profileImg(image)
                 .roles(Collections.singletonList("USER"))
                 .loginType("service")
