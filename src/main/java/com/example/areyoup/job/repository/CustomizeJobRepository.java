a83ed6780a55a227162210d6a451178ec86b9eea
X1n9fU <minkyeong0244@gmail.com>
2024-04-15 15:46:52 +0900

    [chore] JPA member.id로 수정

--- src/main/java/com/example/areyoup/job/repository/CustomizeJobRepository.java
+++ src/main/java/com/example/areyoup/job/repository/CustomizeJobRepository.java
@@ -10,19 +10,19 @@ import java.util.List;
 
 public interface CustomizeJobRepository extends JpaRepository<CustomizeJob, Long> {
     //start - end 사이의 일정 모두 반환
-    @Query("SELECT cj FROM CustomizeJob cj WHERE cj.startDate BETWEEN :start AND :end AND cj.isFixed = true And cj.member = :id")
+    @Query("SELECT cj FROM CustomizeJob cj WHERE cj.startDate BETWEEN :start AND :end AND cj.isFixed = true And cj.member.id = :id")
     List<CustomizeJob> findByStartDateBetweenAndIsFixedIsTrue(
             @Param("start") LocalDate start,
             @Param("end") LocalDate end,
             @Param("id") Long memberId
     );
 
-    @Query("SELECT cj FROM CustomizeJob cj WHERE cj.startTime IS NULL AND cj.isFixed = false And cj.member = :id")
+    @Query("SELECT cj FROM CustomizeJob cj WHERE cj.startTime IS NULL AND cj.isFixed = false And cj.member.id = :id")
     List<CustomizeJob> findAdjustJob(@Param("id") Long memberId);
 
     @Query("SELECT cj FROM CustomizeJob cj WHERE cj.startTime IS NOT NULL " +
             "AND cj.startDate BETWEEN :start AND :end " +
-            "AND cj.isFixed = true And cj.member = :id")
+            "AND cj.isFixed = true AND cj.member.id = :id")
     List<CustomizeJob> findFixedJob(@Param("start") LocalDate start, @Param("end") LocalDate end, @Param("id") Long id);
     CustomizeJob findByNameAndMemberId(String name, Long memberId);
 
--- src/main/java/com/example/areyoup/job/repository/JobRepository.java
+++ src/main/java/com/example/areyoup/job/repository/JobRepository.java
@@ -13,10 +13,10 @@ import java.util.List;
 public interface JobRepository extends JpaRepository<Job, Long> {
     List<Job> findJobsByMemberId(Long memberId);
     @Query(value = "SELECT SUM(TIME_TO_SEC(s.estimated_time) DIV 60) FROM Job s " +
-            "WHERE s.dtype = 'S' AND s.member = :id GROUP BY s.name = :name" , nativeQuery = true)
+            "WHERE s.dtype = 'S' AND s.member_id = :id GROUP BY s.name = :name" , nativeQuery = true)
     Integer getTotalEstimatedTimeByName(@Param("name") String name, @Param("id") Long id);
 
     @Query(value = "SELECT SUM(TIME_TO_SEC(s.estimated_time) DIV 60) FROM Job s " +
-            "WHERE s.dtype = 'D' AND s.member = :id ", nativeQuery = true)
+            "WHERE s.dtype = 'D' AND s.member_id = :id ", nativeQuery = true)
     Integer getLeftTimeFromDefaultJob(@Param("id") Long id);
 }
