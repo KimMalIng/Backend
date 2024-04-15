dd3c7e6dd3208c71bd3bde042d02d7cd34a8dca0
X1n9fU <minkyeong0244@gmail.com>
2024-04-15 14:43:32 +0900

    [chore] Member 패키지 안으로 이동

--- src/main/java/com/example/areyoup/member/profileimage/repository/ProfileImageRepository.java
+++ src/main/java/com/example/areyoup/member/profileimage/repository/ProfileImageRepository.java
@@ -1,6 +1,6 @@
-package com.example.areyoup.profileimage.repository;
+package com.example.areyoup.member.profileimage.repository;
 
-import com.example.areyoup.profileimage.domain.ProfileImage;
+import com.example.areyoup.member.profileimage.domain.ProfileImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
