9fa32bb42cbf449c8ef8d10b4367d970229b17e2
X1n9fU <minkyeong0244@gmail.com>
2024-05-02 15:16:07 +0900

    [fix] cors 설정 http://localhost:3000

--- src/main/java/com/example/areyoup/global/config/WebConfig.java
+++ src/main/java/com/example/areyoup/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
