87616a46d42dce8f6a8a8df1bba0c97eb9d59ca9
X1n9fU <minkyeong0244@gmail.com>
2024-05-02 16:54:55 +0900

    [fix] cors 변경

--- src/main/java/com/example/areyoup/global/config/SecurityConfig.java
+++ src/main/java/com/example/areyoup/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.areyoup.global.cookie.CookieAuthorizationRequestRepository;
 import com.example.areyoup.global.jwt.JwtAuthFilter;
 import com.example.areyoup.global.oAuth2.OAuth2SuccessHandler;
 import com.example.areyoup.global.oAuth2.OAuth2UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Configuration
@@ -27,13 +33,27 @@ public class SecurityConfig {
     private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
 
     private final JwtAuthFilter jwtAuthFilter;
+//    private final WebConfig webConfig;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.httpBasic(HttpBasicConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 //REST API 이므로 basic auth 및 csrf 보안을 사용하지 않음
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+                        @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                        config.setAllowedOrigins(Collections.singletonList("*"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L); //1시간
+                            source.registerCorsConfiguration("/**",config);
+                        return config;
+                    }
+                }))
                 .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 //JWT를 사용하기 때문에 세션을 사용하지 않음
                 .authorizeHttpRequests(
@@ -67,15 +87,15 @@ public class SecurityConfig {
 //                            System.out.println("로그아웃");
 //                            response.sendRedirect("http://localhost:3000");
 //                        }))
-                .oauth2Login(
-                        oauth2 -> oauth2
-                                .authorizationEndpoint(auth -> auth.authorizationRequestRepository(cookieAuthorizationRequestRepository))
-                                .successHandler(successHandler)
-                                //인증 성공시 처리하는 핸들러
-                                .userInfoEndpoint(config -> config.userService(OAuth2UserService))
-                        //oauth2 로그인 성공 후 설정 시작
-                        //사용자 정보 객체를 가져오고 가져왔을 때 어떤 Service  파일을 쓸 것이냐
-                )
+//                .oauth2Login(
+//                        oauth2 -> oauth2
+//                                .authorizationEndpoint(auth -> auth.authorizationRequestRepository(cookieAuthorizationRequestRepository))
+//                                .successHandler(successHandler)
+//                                //인증 성공시 처리하는 핸들러
+//                                .userInfoEndpoint(config -> config.userService(OAuth2UserService))
+//                        //oauth2 로그인 성공 후 설정 시작
+//                        //사용자 정보 객체를 가져오고 가져왔을 때 어떤 Service  파일을 쓸 것이냐
+//                )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 //                .addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class);
         //JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
