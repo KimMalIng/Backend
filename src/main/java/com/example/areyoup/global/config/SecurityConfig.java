6c87a95fcbc889f5935ec90dab9edeebfb28a54d
X1n9fU <minkyeong0244@gmail.com>
2024-05-04 00:52:06 +0900

    [fix] oAuth2 redirect 오류 해결

--- src/main/java/com/example/areyoup/global/config/SecurityConfig.java
+++ src/main/java/com/example/areyoup/global/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -34,7 +35,6 @@ public class SecurityConfig {
     private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
 
     private final JwtAuthFilter jwtAuthFilter;
-//    private final WebConfig webConfig;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -49,6 +49,7 @@ public class SecurityConfig {
                                 //요청에 대한 인가 규칙 설정
                                 request
                                         .requestMatchers(
+                                                new AntPathRequestMatcher("/"),
                                                 new AntPathRequestMatcher("/oauth2/**"),
                                                 new AntPathRequestMatcher("/login/oauth2/code/**"),
                                                 new AntPathRequestMatcher("/users/login/**"),
@@ -62,8 +63,7 @@ public class SecurityConfig {
                                         .anyRequest().authenticated()
 //                        //이 밖에 모든 요청에 대해서 인증 필요
                 )
-//                .formLogin(login ->
-//                        login.loginPage("/login").permitAll())
+                .formLogin(Customizer.withDefaults())
 //                .logout(logout -> logout
 //                        .logoutUrl("/users/logout")
 //                        .deleteCookies("JSESSIONID", "remember-me")
@@ -75,15 +75,15 @@ public class SecurityConfig {
 //                            System.out.println("로그아웃");
 //                            response.sendRedirect("http://localhost:3000");
 //                        }))
-//                .oauth2Login(
-//                        oauth2 -> oauth2
-//                                .authorizationEndpoint(auth -> auth.authorizationRequestRepository(cookieAuthorizationRequestRepository))
-//                                .successHandler(successHandler)
-//                                //인증 성공시 처리하는 핸들러
-//                                .userInfoEndpoint(config -> config.userService(OAuth2UserService))
-//                        //oauth2 로그인 성공 후 설정 시작
-//                        //사용자 정보 객체를 가져오고 가져왔을 때 어떤 Service  파일을 쓸 것이냐
-//                )
+                .oauth2Login(
+                        oauth2 -> oauth2
+                                .authorizationEndpoint(auth -> auth.authorizationRequestRepository(cookieAuthorizationRequestRepository))
+                                .successHandler(successHandler)
+                                //인증 성공시 처리하는 핸들러
+                                .userInfoEndpoint(config -> config.userService(OAuth2UserService))
+                        //oauth2 로그인 성공 후 설정 시작
+                        //사용자 정보 객체를 가져오고 가져왔을 때 어떤 Service  파일을 쓸 것이냐
+                )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 //                .addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class);
         //JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
